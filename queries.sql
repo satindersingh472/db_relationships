e7858c646f73e75ca3f5f92e47a503fd59d08b04
satindersingh <satindersingh472@gmail.com>
2022-09-13 19:06:31 -0600

    finished knowledge check

---
+++ queries.sql
@@ -0,0 +1,4 @@
+INSERT into citizen (address,phone_number,date_of_birth)values('1 fake st','123456123','2000-05-05'),
+('2 fake st','1234567894','1999-01-02');
+
+INSERT into tax_account (date_created,account_number)values('2021-01-01','123456');
